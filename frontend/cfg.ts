05e38b82a7f4b739de6e8fa003b3f964f400eace
mystyuri <yu-myst@yandex.ru>
2025-04-19 16:35:05 +0500

    v0.2.1.5

--- frontend/cfg.ts
+++ frontend/cfg.ts
@@ -3,6 +3,6 @@ const host = url.host;
 const IS_HTTPS = url.protocol.replace(':', '') === 'https';
 
 export const API_URL = `${url}graphql`;
-export const WS_API_URL = `${IS_HTTPS ? 'wss' : 'ws'}://${host}graphql`;
+export const WS_API_URL = `${IS_HTTPS ? 'wss' : 'ws'}://${host}/graphql`;
 
 export const is_git = process.env.GITHUB_PAGES === 'true';
